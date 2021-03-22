50ac7d51c31675f177b94d0ce7bfca80d7e42b15
ngrtp <ngravindra@gmail.com>
2021-03-22 16:52:59 -0400

    lda set up

---
+++ scripts/load_txtdata.py
@@ -0,0 +1,13 @@
+'''
+load_txtdata.py
+  Target documents are txt from articles, while indexed descriptions are
+    in a .xml file
+
+  Instructions:
+    - for extracting txt from articles
+      1. python pkg PyPDF2 OR (quicker) manually sourced
+
+'''
+
+
+if __name__ == '__main__':
